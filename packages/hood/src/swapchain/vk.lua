485a5db8c63e818f1037a03ddc3cd8c4ffd63824
David Cruz <david@codebycruz.com>
2026-02-09 00:16:58 -0800

    refactor(vk): vkswapchain progress

--- packages/hood/src/swapchain/vk.lua
+++ packages/hood/src/swapchain/vk.lua
@@ -1,25 +1,37 @@
 local vk = require("hood-vulkan")
+local ffi = require("ffi")
 
 local VKTexture = require("hood.texture.vk")
 
 ---@class hood.vk.Swapchain
----@field swapchain vk.SwapchainKHR
----@field device vk.Device
+---@field handle vk.ffi.SwapchainKHR
+---@field device hood.vk.Device
+---@field images vk.ffi.Image[] # 1 indexed array of VkImage handles
+---@field currentVkImageIdx integer
 local VKSwapchain = {}
 VKSwapchain.__index = VKSwapchain
 
----@param device vk.Device
+---@param device hood.vk.Device
 ---@param info vk.ffi.SwapchainCreateInfoKHR
 function VKSwapchain.new(device, info)
-	return setmetatable({ device = device, swapchain = vk.createSwapchainKHR(device, info) }, VKSwapchain)
+	local handle = device.handle:createSwapchainKHR(info)
+	local images = device.handle:getSwapchainImagesKHR(handle)
+
+	return setmetatable({ images = images, device = device, handle = handle }, VKSwapchain)
 end
 
 function VKSwapchain:getCurrentTexture()
-	local imageIndex = vk.acquireNextImageKHR(self.device, self.swapchain, math.huge)
-	return VKTexture.new(self.device, imageIndex)
+	local currentVkImageIdx = self.device.handle:acquireNextImageKHR(self.handle, math.huge)
+	local imageHandle = self.images[currentVkImageIdx + 1]
+
+	self.currentVkImageIdx = currentVkImageIdx
+	error("return vktexture here")
+	-- return VKTexture.new(self.device.handle, cImageIndex)
 end
 
 ---@param queue hood.vk.Queue
-function VKSwapchain:present(queue) end
+function VKSwapchain:present(queue)
+	self.device.handle:queuePresentKHR(queue.handle, self.handle, self.vkImageIdx)
+end
 
 return VKSwapchain
